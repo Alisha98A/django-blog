d5c1b1b84f1e493c32a80bc016ea8899349eb5a3
Alisha <alisha@Alishas-MacBook-Air.local>
2024-08-21 17:04:45 +0200

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o)o_1=0*tm5qgz)u&7_nub+jq8c85jkqf0a84vwr6d%sc&r(&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
