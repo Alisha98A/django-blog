a27dfe80f9399d89d70583b7aec013ffda70f135
Alisha <alisha@Alishas-MacBook-Air.local>
2024-09-09 19:52:55 +0200

    add test

--- about/test_forms.py
+++ about/test_forms.py
@@ -5,6 +5,46 @@ from .forms import CollaborateForm
 class TestCollaborateForm(TestCase):
 
     def test_form_is_valid(self):
-        form_data = {'name': 'John Doe', 'email': 'john@example.com', 'message': 'This is a collaboration request'}
-        form = CollaborateForm(form_data)
-        self.assertTrue(form.is_valid())
+        """Test the form with valid data"""
+        form = CollaborateForm({
+            'name': 'Matt',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertTrue(form.is_valid(), msg="The form is not valid when it should be")
+
+    def test_name_is_required(self):
+        """Test for the 'name' field being required"""
+        form = CollaborateForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Name was not provided, but the form is valid"
+        )
+
+    def test_email_is_required(self):
+        """Test for the 'email' field being required"""
+        form = CollaborateForm({
+            'name': 'Matt',
+            'email': '',
+            'message': 'Hello!'
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Email was not provided, but the form is valid"
+        )
+
+    def test_message_is_required(self):
+        """Test for the 'message' field being required"""
+        form = CollaborateForm({
+            'name': 'Matt',
+            'email': 'test@test.com',
+            'message': ''
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Message was not provided, but the form is valid"
+        )
